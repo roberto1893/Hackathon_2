dc8909b0552694c5d730942f6024ee65199416a8
Robert Vlček <roberto1893acs@gmail.com>
2024-05-24 13:48:56 +0200

    popup

---
+++ src/script.js
@@ -0,0 +1,6 @@
+const action = document.getElementById('action');
+const popupForm = document.getElementById('popupForm');
+
+action.addEventListener('click', function () {
+    popupForm.style.display = "flex";
+ })
